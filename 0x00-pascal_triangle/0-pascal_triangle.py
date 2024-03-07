103e528b8456e101dff1563e5dc049ecba3ff501
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2024-03-08 02:13:17 +0300

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+    """
+    Generates Pascal's triangle up to the nth row.
+    Args:
+        n (int): The number of rows in the Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        row = []
+        coefficient = 1
+        for j in range(i + 1):
+            row.append(coefficient)
+            coefficient = coefficient * (i - j) // (j + 1)
+        triangle.append(row)
+
+    return triangle
